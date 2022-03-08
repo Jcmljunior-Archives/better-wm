b00b7f00ca80667a2902436b6a835e9c18564057
Julio Cesar <jcmljunior@gmail.com>
2022-03-08 01:22:47 -0300

    code refactoring

--- bin/up.bash
+++ bin/up.bash
@@ -1,90 +1,71 @@
 #!/usr/bin/env bash
 
-# CONFIGURAÇÕES BÁSICAS PARA A INICIALIZAÇÃO DO GERENCIADOR DE JANELAS.
+# CONFIGURAÇÕES BÁSICAS PARA A INICIALIZAÇÃO DE UM GERENCIADOR DE JANELAS.
+# PROJETO: (https://github.com/jcmljunior/better-wm)
 # AUTHOR: JULIO CESAR <jcmljunior@gmail.com>
 # VERSÃO: 1.0.0
 
-# FORÇA A APRESENTAÇÃO DE ERROS.
-set -euo pipefail
-
-# CONFIGURAÇÕES DE PROJETO.
-export readonly PROJECT_MODE="developer"
-export readonly PROJECT_PATH='/home/USER/.config'
-export readonly PROJECT_DIR='better-wm'
-export readonly PROJECT_PATH="${PROJECT_PATH/USER/$USER}/$PROJECT_DIR"
-
-# CONFIGURAÇÕES DE FUNCIONALIDADES.
-declare -x PROJECT_CONF; PROJECT_CONF=$(cat "${PROJECT_PATH}/config.conf")
-
-# A FUNÇÃO GET_PATH RETORNA O CAMINHO ABSOLUTO DO PROJETO DE DENVOLVIMENTO OU PRODUÇÃO.
-function _get_path()
-{
-    declare -I _path; _path="${PROJECT_PATH/USER/$USER}"
-    [[ "$PROJECT_MODE" == "developer" ]] && echo "${_path/.config/Git}" || echo "$_path"
+# A FUNÇÃO "_get_path" RETORNA O CAMINHO ABSOLUTO DO PROJETO DE ACORDO COM AS CONFIGURAÇÕES
+# DA VARIAVEL "_PROJECT_MODE".
+function _get_path() {
+    [[ "$_PROJECT_MODE" == "developer" ]] && printf "%s/.config/%s\n" "$_PROJECT_PATH" "$_PROJECT_DIR" && return 0
+    [[ "$_PROJECT_MODE" == "production" ]] && printf "%s/Git/%s\n" "$_PROJECT_PATH" "$_PROJECT_DIR" && return 0
+    printf "Failed operation! \n" && return 1
 }
 
-# A FUNÇÃO IN_ARRAY PROCURA POR UM VALOR ESPECIFICO EM UM ARRAY.
-# DEMO: _in_array "O QUE PROCURA?" "AONDE PROCURAR?"
+# A FUNÇÃO "_in_array" PROCURA POR UM VALOR ESPECIFICO EM UM ARRAY.
+# DEMO 01: _in_array "O QUE PROCURA?" "AONDE PROCURAR?"
+# DEMO 02: _in_array "O QUE PROCURA?" "AONDE PROCURAR 01" "AONDE PROCURAR 02" "AONDE PROCURAR 03" [...]
 function _in_array()
 {
-    declare -I _search; _search="$1"
-    declare -Ia _map; mapfile -t _map <<< "${@:2}"
-    
-    for str in "${_map[@]}"; do
-        [[ "$str" =~ .*"$_search".* ]] && return 0
+    declare -- _looking_for; _looking_for="$1"
+    declare -a _looking_map; _looking_map=("${@:2}")
+
+    for str in "${_looking_map[@]}"; do
+        [[ "$str" =~ .*"$_looking_for".* ]] && echo "TRUE" && return 0
     done
-    
-    return 1
+
+    echo "FALSE" && return 1
 }
 
-# A FUNÇÃO AUTOLOAD INICIALIZA TODAS AS ENTRADAS DE ÁREA DE TRABALHO. (XDG)
-# DEFINE OS DIRETÓRIOS A SEREM PESQUISADOS.
-# PERCORRE OS DIRETÓRIOS INFORMADO. (LOOP)
-# DEFINE O SEU RESPECTIVO CONTEÚDO.
-# ALTERA O FORMATO DOS VALORES ENCONTRADOS PARA ARRAY.
-# OBTEM A LISTA DE APLICAÇÕES QUE NÃO DEVEM SER CARREGADAS. (!!!)
-# PERCORRE OS ELEMENTOS CONVERTIDO EM ARRAY. (LOOP)
-# VERIFICA SE O TIPO DE ELEMENTO ENCONTRADO É DO TIPO ARQUIVO.
-# FILTRA O ELEMENTO ATUAL: OBTEM O VALOR DO EXECUTAVEL.
-# EXECUTA O ELEMENTO.
-function _autoload()
-{
-    declare -Ia _dirs; _dirs=(
-        "/etc/xdg/autostart"
-        "${PROJECT_PATH/$PROJECT_DIR}autostart"
-    )
+function _autoload() {
+    declare -I _PROJECT_PATH; _PROJECT_PATH="$_PROJECT_PATH/.config"
+    declare -a _directories
 
-    # VERIFICA A EXISTENCIA DO DIRETÓRIO SETADO MANUALMENTE. (REQUERIDO!)
-    # CRIA O DIRETÓRIO DE ENTRADAS NA PASTA PESSOAL DO USUÁRIO CASO NÃO EXISTA. (PREVINE ERRO DURANTE A LISTAGEM)
-    if [ -d "${_dirs[0]}" ]; then
-        [[ ! -d "${_dirs[1]}" ]] && mkdir "${_dirs[1]}"
+    [[ -d "/etc/xdg/autostart" ]] && _directories[0]="/etc/xdg/autostart" || return 1
+    [[ -d "$_PROJECT_PATH/autostart" ]] && _directories[1]="$_PROJECT_PATH/autostart"
 
-        for dir in "${_dirs[@]}"; do
-            declare -Ix _desktop_entries; _desktop_entries=$(find "$dir" -type f);
-            declare -Ia _desktop_entries_map; mapfile -t _desktop_entries_map <<< "$_desktop_entries"
-            declare -Ix _desktop_entries_not_allowed; _desktop_entries_not_allowed=$(echo -n "$PROJECT_CONF" | grep -e "AUTOSTART_*" | grep -e "_BLOCKED=*" | awk -F '=' '/=/ {print $2}' | sed -e "s| |\n|")
+    for directory in "${_directories[@]}"; do
+        declare -x _desktop_entries; _desktop_entries=$(find "$directory" -type f)
+        declare -a _desktop_entries_map; _desktop_entries_map=($_desktop_entries)
+        declare -- _desktop_entries_not_allowed; _desktop_entries_not_allowed=$(echo -n "$_PROJECT_CONF" | grep -E "AUTOSTART"); _desktop_entries_not_allowed="${_desktop_entries_not_allowed##*=}"
 
-            for item in "${_desktop_entries_map[@]}"; do
-                if [ -f "$item" ]; then
-                    declare -Ix _desktop_entry_exec; _desktop_entry_exec=$(grep -w "Exec=*" "$item" <<< cat); _desktop_entry_exec="${_desktop_entry_exec/Exec=}"
-                    declare -Ifx _in_array; _in_array "$item" "$_desktop_entries_not_allowed">/dev/null || {
-                        printf "%s \n" "$item"
-                    }
-                fi
-            done
+        for application in "${_desktop_entries_map[@]}"; do
+            declare -- _desktop_entry_app; _desktop_entry_app=$(grep -E "Exec=" "$application" <<< cat); _desktop_entry_app="${_desktop_entry_app##*Exec=}"
+            declare -f _check_str; _check_str=$(_in_array "$application" "$_desktop_entries_not_allowed")
+
+            [[ "$_check_str" == "TRUE" ]] && continue;
+            [[ ! -f "$application" ]] && continue;
+            
+            echo "$application - $_check_str"
         done
-
-    fi
-
-    exit 0
+    done
 }
 
-# A FUNÇÃO MAIN INICIALIZA TODOS OS COMPONENTES.
+# A FUNÇÃO "_main" INICIALIZA TODOS OS COMPONENTES.
 function _main()
 {
-    declare -Ix _use_autoload; _use_autoload=$(echo -n "$PROJECT_CONF" | grep -e "AUTOSTART_*" | grep -e "_ENABLED=*" | awk -F '=' '/=/ {print $2}')
-    [[ "$_use_autoload" == "TRUE" ]] && _autoload;
+    declare -- _use_autoload; _use_autoload=$(echo -n "$_PROJECT_CONF" | grep -E "AUTOSTART_ENABLE"); _use_autoload="${_use_autoload##*=}"
+    [[ "$_use_autoload" == "TRUE" ]] && _autoload
 }
+
+# DEFINIÇÕES DE PROJETO.
+declare -- _PROJECT_MODE; _PROJECT_MODE="developer"
+declare -- _PROJECT_PATH; _PROJECT_PATH="/home/USER"; _PROJECT_PATH="${_PROJECT_PATH/USER/$USER}"
+declare -- _PROJECT_DIR; _PROJECT_DIR="better-wm"
+
+# DEFINIÇÕES DE FUNCIONALIDADES.
+declare -- _PROJECT_CONF; _PROJECT_CONF=$(cat "$(_get_path)""/config.conf")
 
 _main
 
